575dbeb88e7637f10b35136576dc90ffeccb20b0
charles trudeau <chtru70@ulaval.ca>
2025-03-16 18:11:46 -0400

    question2 fini pas corrig/

--- equipe140.py
+++ equipe140.py
@@ -51,3 +51,48 @@ detC = np.linalg.det(J(lastiterC, C))
 print(detA)
 print(detB)
 print(detC)
+
+En_A = np.linalg.norm(SolA[:,1:] - SolA[:,:-1], axis=0)
+En_B = np.linalg.norm(SolB[:,1:] - SolB[:,:-1], axis=0)
+En_C = np.linalg.norm(SolC[:,1:] - SolC[:,:-1], axis=0)
+
+plt.figure(1)
+plt.semilogy(En_A, label='En_A = ∥⃗xn+1 − ⃗xn∥')
+plt.semilogy(En_B, label='En_B = ∥⃗xn+1 − ⃗xn∥')
+plt.semilogy(En_C, label='En_C = ∥⃗xn+1 − ⃗xn∥')
+plt.xlabel("Itération n")
+plt.ylabel("Erreur |x_n+1 - x_n|")
+plt.title("Évolution de l'erreur en fonction du nombre d'itération en semi-log")
+plt.legend()
+
+A = np.array([(1, 1), (0, 0.73205), (-0.73205, 0)])
+B = np.array([(-1, 0), (-1.0066, 0.1147), (-1.136, 0.50349)])
+C = np.array([(0, 1), (-0.0112, 1.149247), (-0.0465, 1.301393)])
+
+
+hA, kA, rA = SolA[:,-1]
+hB, kB, rB = SolB[:,-1]
+hC, kC, rC = SolC[:,-1]
+
+fig, ax = plt.subplots(figsize=(8, 8))  
+ax.set_aspect('equal') 
+
+ax.scatter(A[:, 0], A[:, 1], label="Données A", color='blue', marker='o')
+ax.scatter(B[:, 0], B[:, 1], label="Données B", color='red', marker='s')
+ax.scatter(C[:, 0], C[:, 1], label="Données C", color='green', marker='d')
+
+cercle_A = plt.Circle((hA, kA), rA, color='blue', fill=False, linestyle='dashed', linewidth=1.5, label="Cercle A")
+cercle_B = plt.Circle((hB, kB), rB, color='red', fill=False, linestyle='dashed', linewidth=1.5, label="Cercle B")
+cercle_C = plt.Circle((hC, kC), rC, color='green', fill=False, linestyle='dashed', linewidth=1.5, label="Cercle C")
+
+ax.add_patch(cercle_A)
+ax.add_patch(cercle_B)
+ax.add_patch(cercle_C)
+
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Cercle passant par les points des jeux A, B et C")
+ax.legend()
+ax.grid(True, linestyle="--", linewidth=0.5)
+
+plt.show()
