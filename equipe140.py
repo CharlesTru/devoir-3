77c474da19ba416993144f2da86dbdeec5675268
charles trudeau <chtru70@ulaval.ca>
2025-03-16 16:00:09 -0400

    fonctionne pas

--- equipe140.py
+++ equipe140.py
@@ -1,22 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from newton import newton
-
-def f(x, points):
-    h, k, r = x
-    return np.array([
-        (points[0][0] - h)**2 + (points[0][1] - k)**2 - r**2,
-        (points[1][0] - h)**2 + (points[1][1] - k)**2 - r**2,
-        (points[2][0] - h)**2 + (points[2][1] - k)**2 - r**2
-    ])
-
+    
+def f(x,points):
+        h, k, r = x
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        x3, y3 = points[2]
+        f1 = (x1 - h)**2 + (y1 - k)**2 - r**2
+        f2 = (x2 - h)**2 + (y2 - k)**2 - r**2
+        f3 = (x3 - h)**2 + (y3 - k)**2 - r**2
+        return np.array([f1, f2, f3])
 
 def J(x, points):
-    h, k, r = x
-    return np.array([
-        [-2 * (points[0][0] - h), -2 * (points[0][1] - k), -2 * r],
-        [-2 * (points[1][0] - h), -2 * (points[1][1] - k), -2 * r],
-        [-2 * (points[2][0] - h), -2 * (points[2][1] - k), -2 * r]])
+        h, k, r = x
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        x3, y3 = points[2]
+        return np.array([
+            [-2*(x1 - h), -2*(y1 - k), -2*r],
+            [-2*(x2 - h), -2*(y2 - k), -2*r],
+            [-2*(x3 - h), -2*(y3 - k), -2*r]
+        ])
 
 donnée = {
     "A": [(1, 1), (0, 0.73205), (-0.73205, 0)],
@@ -28,8 +33,5 @@ tolr = 10**-10
 nmax = 100
 np.set_printoptions(precision=16)
 
-for key, points in donnée.items():
-    (x1, y1), (x2, y2), (x3, y3) = points
-    solution = newton(f(x0, points), J(x0, points), x0, tolr, nmax)
-
-print(solution)
+for key, item in donnée.items():
+    newton(f(x0, item), J(x0, item), x0, tolr, nmax)
